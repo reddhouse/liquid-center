5a95b58b90fe7392b3e7dfb1a1eff7efd8202108
JMT <reddhouse@gmail.com>
2019-02-11 20:27:25 -0800

    Configure nextjs with styled-components

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Home() {
+  return (
+    <Title>Liquid Center</Title>
+  )
+}
+
+export default Home
+
+/***************************** Styled Components *****************************/
+
+const Title = styled.h1`
+  color: blue;
+  font-size: 50px;
+`
