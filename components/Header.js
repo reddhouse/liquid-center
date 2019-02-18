9740082bda58e46162354ad3c4791abeda3cf0db
JMT <reddhouse@gmail.com>
2019-02-17 20:53:52 -0800

    Demo responsive background images in header

--- components/Header.js
+++ components/Header.js
@@ -32,6 +32,38 @@ const Container = styled.div`
   flex: 0 0 auto;
   display: flex;
   justify-content: flex-start;
+
+  ${'' /* Use large image by default */}
+  background-image: url("/static/l-pic.jpg");
+  background-position: center center;
+  background-size: cover;
+  min-height: 30vh;
+
+  ${'' /* When on tablet, use medium image, except if retina display, bump to large */}
+  ${props => props.theme.media.tablet`
+    background-image: url("/static/m-pic.jpg");
+    background-image: -webkit-image-set(
+            url("/static/m-pic.jpg") 1x,
+            url("/static/l-pic.jpg") 2x
+    );
+    background-image: image-set(
+            url("/static/m-pic.jpg") 1x,
+            url("/static/l-pic.jpg") 2x
+    );
+  `}
+
+${'' /* When on phone, use small image, except if retina display, bump to medium */}
+  ${props => props.theme.media.phone`
+    background-image: url("/static/s-pic.jpg");
+    background-image: -webkit-image-set(
+            url("/static/s-pic.jpg") 1x,
+            url("/static/m-pic.jpg") 2x
+    );
+    background-image: image-set(
+            url("/static/s-pic.jpg") 1x,
+            url("/static/m-pic.jpg") 2x
+    );
+  `}
 `
 
 const NavItem = styled.div`
--- pages/_app.js
+++ pages/_app.js
@@ -42,7 +42,7 @@ const sizes = {
 
 const media = Object.keys(sizes).reduce((acc, label) => {
   acc[label] = (...args) => css`
-    @media (min-width: ${sizes[label] / 16}em) {
+    @media (max-width: ${sizes[label] / 16}em) {
       ${css(...args)}
     }
   `
