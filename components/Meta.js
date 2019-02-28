273d181c841c52b1ffe9f3906b6f2a926e500169
JMT <reddhouse@gmail.com>
2019-02-28 14:00:40 -0800

    Add final version of header image + favicon

--- components/Meta.js
+++ components/Meta.js
@@ -7,6 +7,7 @@ export default (props) => (
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
       <title>{props.title}</title>
+      <link rel="icon" type="image/x-icon" href="../static/favicon.ico" />
     </Head>
     <style jsx global>
       {`
