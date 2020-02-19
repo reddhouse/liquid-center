8fe09fc551a7fab27b5f74f65205f74d2f9d1354
JMT <reddhouse@gmail.com>
2020-02-19 12:08:03 -0800

    Remove boilerplate code not necessary with Next9

--- pages/_app.js
+++ pages/_app.js
@@ -10,16 +10,6 @@ Router.events.on('routeChangeComplete', (url) => {
 })
 
 class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {}
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
-    }
-
-    return { pageProps }
-  }
-
   render () {
     const { Component, pageProps } = this.props
 
